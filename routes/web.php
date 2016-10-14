d604960ad6dcf9d0e90a1e11eaab538c2157bc0e
camilo912 <cam.912@hotmail.com>
2016-10-14 12:00:49 -0500

    ahora si routes para tony

--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,4 @@ Route::get('/home', 'HomeController@index');
 
 Route::get('/habitacion', 'ControladorFrontEnd@habitacion');
 
-Route::get('/arrendador', 'ControladorFrontEnd@arrendador')
+Route::get('/arrendador', 'ControladorFrontEnd@arrendador');
