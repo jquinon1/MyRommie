609a5ddc85873fd6905816762969ac2cb46483dd
ccartag4 <ccartag4@eafit.edu.co>
2016-10-14 17:46:31 -0500

    parte de habitacion

---
+++ resources/views/habitacion.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <style>
+        .card {
+            box-shadow: 0 4px 8px 0 rgba(0,0,0,0.0);
+            transition: 0.3s;
+            width:40%;
+        }
+
+        .card:hover {
+            box-shadow: 0 8px 16px 0 rgba(0,0,0,0.0);
+        }
+
+        .container {
+            padding: 2px 10px;
+        }
+    </style>
+</head>
+<body background="images/fpantalla.jpg">
+
+<div class="w3-card-24">
+    <img src="images/hab.jpg" >
+    <div class="w3-container w3-center">
+        <h4>Villas EAFIT</h4>
+        <br>
+        <p>500$ COP</p>
+    </div>
+</div>
+
+
+</body>
+</html>
