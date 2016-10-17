fa3a1bf88f1823a8f63ac48045d53f5c0b1b98a6
jzulua70 <juandizg11@hotmail.es>
2016-10-17 15:52:41 -0500

    Configuracion estudiante

---
+++ app/Http/Controllers/EstudianteController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Habitacion;
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use Illuminate\Http\Request;
+use Laracasts\Flash\Flash;
+use App\Http\Requests;
+use App\Http\Requests\EstudianteRequest;
+use App\User;
+
+
+class EstudianteController extends Controller
+{
+    public function store(EstudianteRequest $request){
+        $user = new Estudiante($request->all());
+        if ($request->precio != "") {
+            return view('welcome');
+        } else {
+            Flash::warning("No se pudo registrar");
+            return reditec()->route('users.create');
+        }
+        Flash::success("Se ha registrado " . $user->name . " existosamente");
+        return view('welcome');
+    }
+}
