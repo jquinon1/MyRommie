e856339c819fd2120ab733e09c9ad2d6a8b6c117
ccartag4 <ccartag4@eafit.edu.co>
2016-10-17 22:47:06 -0500

    acerca de y contactanos terminado

---
+++ resources/views/acercade.blade.php
@@ -0,0 +1,43 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Acerca De</div>
+                    <div class="panel-body">
+                        <form class="form-horizontal" >
+                            <div class="form-group">
+                                <label for="nombre" class="col-md-4 control-label">Correo </label>
+                                <div class="col-md-6">
+                                    <output id="nombre" type="email" class="form-control" name="nombre" >myrommie@enterpise.com</output>
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label for="nombre" class="col-md-4 control-label">Telefono </label>
+                                <div class="col-md-6">
+                                    <output id="nombre" type="email" class="form-control" name="nombre" >1800myrommie</output>
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label for="nombre" class="col-md-4 control-label">Fax </label>
+                                <div class="col-md-6">
+                                    <output id="nombre" type="email" class="form-control" name="nombre" >754821630</output>
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label for="nombre" class="col-md-4 control-label">Direccion </label>
+                                <div class="col-md-6">
+                                    <output id="nombre" type="email" class="form-control" name="nombre" >Centro Comercial Eafit oficina 401</output>
+                                </div>
+                            </div>
+
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
--- resources/views/infoarrendador.blade.php
+++ resources/views/infoarrendador.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-    <!--<div class="container">
+   <div class="container">
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
                 <div class="panel panel-default">
@@ -52,25 +52,6 @@
                                 </div>
                             </div>
                         </form>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>-->
-
-    <div class="container">
-        <div class="row">
-            <div class="col-md-8 col-md-offset-2">
-                <div class="panel panel-default">
-                    <div class="panel-heading">Acerca De</div>
-                    <div class="panel-body">
-                        <form class="form-horizontal" >
-                            <h4>
-                                MyRommie empezó como una idea de 4 estudiantes de la universidad de EAFIT, con el propósito de realizar un proyecto que solucionara un problema que esté vigente en el mundo actual, fue así como Craig David Cartagena, Jhonatán Quiñonez Ávila, Juan Camilo Henao Salazar y Juan Diego Zuluaga. Pensaron en crear una página web que ayudara a los estudiantes de las universidades a buscar un lugar donde hospedarse mientras estén realizando sus estudios, teniendo en cuenta los detalles que hagan que sea una página con información completo y de fácil uso para los usuarios.
-                            <h4>
-
-
-                         </form>
                     </div>
                 </div>
             </div>
