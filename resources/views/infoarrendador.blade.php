9ed01a5eccfa0005ed4e8b789f95929d2c45b377
ccartag4 <ccartag4@eafit.edu.co>
2016-10-17 22:40:09 -0500

    acerca de terminado

--- resources/views/infoarrendador.blade.php
+++ resources/views/infoarrendador.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
+    <!--<div class="container">
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
                 <div class="panel panel-default">
@@ -13,7 +13,7 @@
                                 <label for="nombre" class="col-md-4 control-label">Nombre Completo: </label>
 
                                 <div class="col-md-6">
-                                    <output id="nombre" type="email" class="form-control" name="nombre" >
+                                    <output id="nombre" type="email" class="form-control" name="nombre" ></output>
                                 </div>
                             </div>
 
@@ -21,7 +21,7 @@
                                 <label for="numero" class="col-md-4 control-label">Numero: </label>
 
                                 <div class="col-md-6">
-                                    <output id="numero" type="email" class="form-control" name="numero" >
+                                    <output id="numero" type="email" class="form-control" name="numero" ></output>
                                 </div>
                             </div>
 
@@ -29,7 +29,7 @@
                                 <label for="Correo" class="col-md-4 control-label">Correo: </label>
 
                                 <div class="col-md-6">
-                                    <output id="Correo" type="email" class="form-control" name="Correo" >
+                                    <output id="Correo" type="email" class="form-control" name="Correo" ></output>
                                 </div>
                             </div>
 
@@ -37,7 +37,7 @@
                                 <label for="Direccion" class="col-md-4 control-label">Direccion: </label>
 
                                 <div class="col-md-6">
-                                    <output id="Direccion" type="email" class="form-control" name="Direccion" >
+                                    <output id="Direccion" type="email" class="form-control" name="Direccion" ></output>
                                 </div>
                             </div>
 
@@ -45,10 +45,10 @@
                                 <label for="info" class="col-md-4 control-label">Informacion Adicional: </label>
 
                                 <div class="col-md-6">
-                                    <output id="info" type="email" class="form-control" name="info" >
+                                    <output id="info" type="email" class="form-control" name="info" ></output>
                                 </div>
                             </div>
-                            
+
                                 </div>
                             </div>
                         </form>
@@ -56,6 +56,26 @@
                 </div>
             </div>
         </div>
+    </div>-->
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Acerca De</div>
+                    <div class="panel-body">
+                        <form class="form-horizontal" >
+                            <h4>
+                                MyRommie empezó como una idea de 4 estudiantes de la universidad de EAFIT, con el propósito de realizar un proyecto que solucionara un problema que esté vigente en el mundo actual, fue así como Craig David Cartagena, Jhonatán Quiñonez Ávila, Juan Camilo Henao Salazar y Juan Diego Zuluaga. Pensaron en crear una página web que ayudara a los estudiantes de las universidades a buscar un lugar donde hospedarse mientras estén realizando sus estudios, teniendo en cuenta los detalles que hagan que sea una página con información completo y de fácil uso para los usuarios.
+                            <h4>
+
+
+                         </form>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
+
 
 @endsection
