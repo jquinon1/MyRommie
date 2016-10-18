75ec8ade27d1304c163aa5d2a9957ad5bcd550ba
ccartag4 <ccartag4@eafit.edu.co>
2016-10-17 21:44:01 -0500

    habitacion casi lista

--- resources/views/habitacion.blade.php
+++ resources/views/habitacion.blade.php
@@ -8,8 +8,148 @@
 
 </head>
 <body>
-
 <style>
+    *{margin: 0; padding: 0}
+    body {
+        background: whitesmoke;
+        text-align: center;
+
+    }
+    #contenedor{
+        text-align: left;
+        margin: auto;
+        width: 100%;
+    }
+    #cabecera{
+        background-color: #cccccc;
+        clear: both;
+
+        text-align:center;
+    }
+    #cuerpo{
+        margin: auto;
+    }
+    #lateral{
+        background-color: whitesmoke;
+        width: 33.3%;
+        float:left;
+        text-align:center;
+    }
+    #otrolado{
+        background-color: whitesmoke;
+        float: right;
+        width: 33.3%;
+        text-align:center;
+        border-bottom-color: grey;
+    }
+    #principal{
+        background-color: whitesmoke;
+        float: left;
+        width: 33.3%;
+        text-align:center;
+    }
+    #pie{
+        background-color: #cccccc;
+        clear: both;
+        text-align:center;
+    }
+
+    div.img {
+        border:1px solid #ccc;
+    }
+
+    div.img:hover {
+        border:1px solid #777;
+    }
+
+
+
+
+    * {
+
+        padding: 1px;
+        box-sizing: border-box;
+    }
+
+
+
+</style>
+
+<!doctype html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Ejemplo</title>
+    <link rel="stylesheet" href="ejemplo.css">
+</head>
+<body>
+<div id="contenedor">
+    <div id="cabecera">
+        HABITACIONES
+    </div>
+    <div id="cuerpo">
+        <div id="lateral">
+            <div class="img">
+                <div class="card-image">
+                    <img src="images/hab2.jpg" alt="hab1" width="600" height="400">
+                </div>
+                <div class="desc" style="color: dimgrey;"><font size="5">Villas Eafit</font></div>
+                <div class="desc" style="color: dimgrey">500.000$ COP</div>
+                <div class="desc" style="color: dimgrey">Ubicado alfrente de eafit con habitacion pero sin cama </div>
+            </div>
+            <div class="img">
+                <div class="card-image">
+                    <img src="images/hab3.jpg" alt="hab1" width="600" height="400">
+                </div>
+                <div class="desc" style="color: dimgrey;"><font size="5">Villas Eafit</font></div>
+                <div class="desc" style="color: dimgrey">500.000$ COP</div>
+                <div class="desc" style="color: dimgrey">Ubicado alfrente de eafit con habitacion pero sin cama </div>
+            </div>
+        </div>
+        <div id="otrolado">
+            <div class="img">
+                <div class="card-image">
+                    <img src="images/hab4.jpg" alt="hab1" width="600" height="400">
+                </div>
+                <div class="desc" style="color: dimgrey;"><font size="5">Villas Eafit</font></div>
+                <div class="desc" style="color: dimgrey">500.000$ COP</div>
+                <div class="desc" style="color: dimgrey">Ubicado alfrente de eafit con habitacion pero sin cama </div>
+            </div>
+            <div class="img">
+                <div class="card-image">
+                    <img src="images/hab5.jpg" alt="hab1" width="600" height="400">
+                </div>
+                <div class="desc" style="color: dimgrey;"><font size="5">Villas Eafit</font></div>
+                <div class="desc" style="color: dimgrey">500.000$ COP</div>
+                <div class="desc" style="color: dimgrey">Ubicado alfrente de eafit con habitacion pero sin cama </div>
+            </div>
+        </div>
+        <div id="principal">
+            <div class="img">
+                <div class="card-image">
+                    <img src="images/hab6.jpg" alt="hab1" width="600" height="400">
+                </div>
+                <div class="desc" style="color: dimgrey;"><font size="5">Villas Eafit</font></div>
+                <div class="desc" style="color: dimgrey">500.000$ COP</div>
+                <div class="desc" style="color: dimgrey">Ubicado alfrente de eafit con habitacion pero sin cama </div>
+            </div>
+            <div class="img">
+                <div class="card-image">
+                    <img src="images/hab9.jpg" alt="hab1" width="600" height="400">
+                </div>
+                <div class="desc" style="color: dimgrey;"><font size="5">Villas Eafit</font></div>
+                <div class="desc" style="color: dimgrey">500.000$ COP</div>
+                <div class="desc" style="color: dimgrey">Ubicado alfrente de eafit con habitacion pero sin cama </div>
+            </div>
+        </div>
+    <div id="pie">
+        MYROMMIE
+    </div>
+</div>
+</body>
+</html>
+
+<!--<style>
     div.img {
         border:1px solid #ccc;
     }
@@ -157,7 +297,7 @@
  </div>
 </div>
 
- <div class="clearfix"></div>
+ <div class="clearfix"></div>-->
 </body>
 </html>
 
