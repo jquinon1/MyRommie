45a50366707885a35034f08987e7ae1c234602d1
jquinoz <jquinon1@eafit.edu.co>
2016-10-25 16:51:23 -0500

    se arreglan problemas con el seed de habitaciones

--- database/migrations/2016_10_14_214309_crear_tabla_habitacion.php
+++ database/migrations/2016_10_14_214309_crear_tabla_habitacion.php
@@ -16,15 +16,15 @@ class CrearTablaHabitacion extends Migration
       Schema::create('habitaciones', function (Blueprint $table) {
         $table->increments('id');
         $table->string('direccion')->nullable();
-        $table->float('longitud', 25, 20)->nullable();
         $table->float('latitud', 25, 20)->nullable();
+        $table->float('longitud', 25, 20)->nullable();
         $table->string('precio');
         $table->double('calificacion',2,1)->default(0.0);
         $table->integer('numero_votos')->default(0);
         $table->enum('estado',['ocupado','desocupado'])->default('desocupado');
         $table->text('descripcion')->nullable();
-        $table->integer('user_id')->unsigned()->nullable();
-        $table->integer('ubicacion_id')->unsigned()->nullable();
+        $table->integer('user_id')->unsigned();
+        $table->integer('ubicacion_id')->unsigned();
 
 
         $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
