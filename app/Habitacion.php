bd9a4ef50017d09aadc63fb223d0e7538811a522
jquinoz <jquinon1@eafit.edu.co>
2016-11-11 16:30:49 -0500

    cambios

--- app/Habitacion.php
+++ app/Habitacion.php
@@ -31,20 +31,4 @@ class Habitacion extends Model
         return $this->hasMany('App\Oferta');
     }
 
-    public function scopeCercaDe($query,$universidad){
-        $query = DB::select("select * from habitaciones join habitacion_universidad on habitaciones.id = habitacion_universidad.habitacion_id join universidades on universidades.id = habitacion_universidad.universidad_id where nombre LIKE '%$universidad%' and estado = 'desocupado'");
-        return $query;
-    }
-
-    public function scopeSearch($query,$universidad){
-        // $universi = DB::select('select id as universidad from universidades where nombre like "%$universidad%"');
-        // $pivot = DB::select('select habitacion_id from habitacion_universidad where universidad_id in "$universi"');
-        // return $query->where('estado','=','desocupado')->whereIn('id',$pivot);
-        //->join('universidades','universidades.id','=','habitacion_universidad.universidad_id')->where('universidades.nombre','LIKE',"%$universidad%");
-        // return $universidad = DB::table('universidades')->join('habitacion_universidad','universidades.id','=','habitacion_universidad.universidad_id')->join('habitaciones','habitaciones.id','=','habitacion_universidad.habitacion_id')->where('nombre','LIKE',"%$universidad%")->where('estado','=','desocupado');
-        // // return DB::table('habitaciones')
-        // ->where('estado','=','desocupado');
-        return $query->join('habitacion_universidad','habitacion_universidad.habitacion_id','=','habitaciones.id')->where('estado','=','desocupado')->join('universidades','universidades.id','=','habitacion_universidad.universidad_id')->where('nombre','LIKE',"%$universidad%");
-        
-    }
 }
