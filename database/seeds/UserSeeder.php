13c52fd471e6182139c3c1dc490a11919045dd7e
jquinon1 <jquinon1@eafit.edu.co>
2016-11-18 09:27:20 -0500

    cambios

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -11,6 +11,18 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        factory(App\User::class,80)->create();
+    	DB::table('users')->insert([
+    		'nombre' => 'Jhonatan',
+    		'apellido' => 'Avila',
+    		'email' => 'jhonaqasistemas@gmail.com',
+    		'password' => bcrypt('myrommie'),
+    		'tipo_id' => 'CC',
+    		'numId' =>	'1017240830',
+    		'genero' => 'hombre',
+    		'tipo_usuario' => 'admin',
+    		'token' => str_random(30),
+    		'activated' => true
+    		]);
+    	factory(App\User::class,80)->create();
     }
 }
