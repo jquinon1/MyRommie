b6d5d03f44702f2df252892d149ae24d186cc70a
jquinoz <jquinon1@eafit.edu.co>
2016-11-18 08:05:26 -0500

    cambios

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,13 +11,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
-        // $this->call(UserSeeder::class);
+//        $this->call(UsersTableSeeder::class);
+        $this->call(UserSeeder::class);
         $this->call(UbicacionSeeder::class);
         $this->call(UniversidadSeeder::class);
-        // $this->call(HabitacionSeeder::class);
-        // $this->call(ImagenesSeeder::class);
+        $this->call(HabitacionSeeder::class);
+        $this->call(ImagenesSeeder::class);
         $this->call(CaracteristicaSeeder::class);
-        // $this->call(Habitacion_UniversidadSeeder::class);
+//        $this->call(Habitacion_UniversidadSeeder::class);
     }
 }
